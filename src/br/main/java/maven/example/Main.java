0873df2fe32729be462867e87561888094e27763
Luan <luansilvf@gmail.com>
2026-06-29 18:59:08 -0300

    teste

--- src/br/main/java/maven/example/Main.java
+++ src/br/main/java/maven/example/Main.java
@@ -22,7 +22,6 @@ public class Main {
 
             switch (escolha) {
                 case 1:
-                    cadastrarLivros();
                     break;
 
                 case 2:
--- src/br/main/java/maven/example/entity/Livro.java
+++ src/br/main/java/maven/example/entity/Livro.java
@@ -2,12 +2,8 @@ package maven.example.entity;
 
 public class Livro {
     public int ano;
-    public int ano;
-    public int ano;
-    public int ano;
     private String titulo;
     private String autor;
-    private int ano;
 
     public Livro(String titulo, String autor, int ano) {
         this.titulo = titulo;
@@ -19,6 +15,8 @@ public class Livro {
         System.out.println("Título: " + this.titulo);
         System.out.println("Autor(a): " + this.autor);
         System.out.println("Ano de publicação: " + this.ano);
+        System.out.println("Ano de publicação: " );
+        System.out.println("Ano de publicação:cxcxc " );
     }
 
 
